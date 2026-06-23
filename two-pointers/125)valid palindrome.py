4797b28b0145aabcd0f5043419328aeb4eba6ad5
kani060 <kanisghavarssini2006@gmail.com>
2026-06-23 16:13:54 +0530

    Add palindrome checking functionality
    Implement palindrome checking function in Solution class.

---
+++ two-pointers/125)valid palindrome.py
@@ -0,0 +1,18 @@
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+Given a string s, return true if it is a palindrome, or false otherwise.
+Example 1:
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s=s.lower()
+        y=[]
+        for ch in s:
+            if ch.isalnum():
+                y.append(ch)
+        if y==y[::-1]:
+           return True
+        else:
+            return False
