8f11b10acb4ff83650c6c216af606a4fdf9b6289
kani060 <kanisghavarssini2006@gmail.com>
2026-06-23 16:20:01 +0530

    Create 1768)merge string alternatively.py

---
+++ two-pointers/1768)merge string alternatively.py
@@ -0,0 +1,16 @@
+# You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+# Return the merged string.
+# Example 1:
+# Input: word1 = "abc", word2 = "pqr"
+# Output: "apbqcr"
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        x=[]
+        i=0
+        while i<len(word1) or i<len(word2):
+            if i<len(word1):
+                x.append(word1[i])
+            if i<len(word2):
+                x.append(word2[i])
+            i+=1
+        return "".join(x)
