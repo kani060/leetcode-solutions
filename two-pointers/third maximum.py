fd92d8352f4cacaa61ba870c9addd6e011e1fda8
kani060 <kanisghavarssini2006@gmail.com>
2026-06-23 13:00:58 +0530

    Create third maximum.py

---
+++ two-pointers/third maximum.py
@@ -0,0 +1,10 @@
+# Given an integer array nums, return the third distinct maximum number in this array. If the third maximum does not exist, return the maximum number.
+
+ class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        nums=list(set(nums))
+        nums.sort(reverse=True)
+        if len(nums)<3:
+            return nums[0]
+        else:
+            return nums[2]
