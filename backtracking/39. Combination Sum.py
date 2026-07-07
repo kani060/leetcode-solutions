a47315679bc12d1c1435fa93a7e8c2222c0d4629
kani060 <kanisghavarssini2006@gmail.com>
2026-07-07 20:37:35 +0530

    Create 39. Combination Sum.py

---
+++ backtracking/39. Combination Sum.py
@@ -0,0 +1,23 @@
+# Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+
+# The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the frequency of at least one of the chosen numbers is different.
+
+# The test cases are generated such that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+
+ class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        ans=[]
+        path=[]
+        def f(i,target):
+            if i==len(candidates):
+                return
+            if target==0:
+                ans.append(path[:])
+                return
+            if candidates[i]<=target:
+                path.append(candidates[i])
+                f(i,target-candidates[i])
+                path.pop()
+            f(i+1,target)
+        f(0,target)
+        return ans
